cfd27b8f67b0f31fe7b40916f0f06ca6127185c2
sumpumm <samarpan69.piya@gmail.com>
2024-12-03 19:44:36 +0545

    boiler plates created

--- petstore/views.py
+++ petstore/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def HomePage(request):
+    return render(request,'index.html')
+
+def AboutUs(request):
+    return render(request,'Aboutus.html')
